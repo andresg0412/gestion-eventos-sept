e76f6e37a7a09ec8058ab1379146ea9a91dbae52
German Andres Gamboa Portilla <andres.gamboa.9504@gmail.com>
2024-09-13 20:31:56 -0500

    New: Creación proyecto base frontend. New: Configuración Docker

--- backend/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    preset: 'jest',
-    testEnvironment: 'node',
-    moduleNameMapper: {
-        '^@/(.*)$': '<rootDir>/src/$1',
-    },
-    setupFilesAfterEnv: ['./jest.setup.js'],
-};
