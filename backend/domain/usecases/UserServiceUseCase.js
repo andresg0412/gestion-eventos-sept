8dbb8ed26f5698e64188c53c6998c2a05f3a1df3
German Andres Gamboa Portilla <andres.gamboa.9504@gmail.com>
2024-09-16 16:33:39 -0500

    New: Configuración archivo CI/CD GitHub Actions Modify: Ajuste de manejo de respuestas y errores en User

--- backend/domain/usecases/UserServiceUseCase.js
+++ backend/domain/usecases/UserServiceUseCase.js
@@ -6,6 +6,9 @@ class UserServiceUseCase {
         try {
             const connection = await this.UserRepository.connectdb();
             const users = await this.UserRepository.getAllUsers();
+            if (!users) {
+                return { status: 404, body: { message: 'No se encontraron usuarios' } };
+            }
             return { status: 200, body: users };
         } catch (error) {
             return { status: 500, body: { message: 'Error al obtener usuarios use case' } };
@@ -14,10 +17,16 @@ class UserServiceUseCase {
     async createUser({ username, email, password }) {
         try {
             const connection = await this.UserRepository.connectdb();
+            if (connection === null) {
+                throw { status: 500, body: { message: 'Error al conectar a la base de datos' } };
+            }
             const user = await this.UserRepository.createUser({ username, email, password });
-            return { status: 200, body: user };
+            if (!user) {
+                throw { status: 500, body: { message: 'Error al crear usuario' } };
+            }
+            return { status: 200, body: { message: 'Usuario creado con éxito'} };
         } catch (error) {
-            return { status: 500, body: { message: 'Error al crear usuario use case' } };
+            throw { status: 500, body: { message: 'Error al crear usuario' } };
         }
     }
     updateUser({ id, username, email, password }) {
