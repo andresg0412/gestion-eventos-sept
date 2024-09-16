7ce3562c666a49a72f477cce00daac3e37e1cc7d
German Andres Gamboa Portilla <andres.gamboa.9504@gmail.com>
2024-09-15 21:08:38 -0500

    Modify: Ajustes para docker y conexión con AWS RDS

--- backend/infrastructure/database/database.js
+++ backend/infrastructure/database/database.js
@@ -3,9 +3,9 @@ require('dotenv').config({ path: '.env' });
 
 const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
 
-const pool = await mysql.createPool({
+const pool = mysql.createPool({
     host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 3306,
+    port: parseInt(process.env.DB_PORT) || 3306,
     user: process.env.DB_USER || 'root',
     password: process.env.DB_PASSWORD || '1234',
     database: process.env.DB_NAME || 'proyecto-eventos',
@@ -15,4 +15,4 @@ pool.on('error', (err) => {
     console.error(err);
 });
 
-module.exports = pool
+module.exports = pool;
--- backend/testDbConnection.js
+++ backend/testDbConnection.js
@@ -1,12 +1,13 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config({ path: '.env' });
 
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
+const { DB_HOST, DB_PORT, DB_USER, DB_PASSWORD, DB_NAME } = process.env;
 
 async function testConnection() {
     try {
         const connection = await mysql.createConnection({
-            host: DB_HOST,
+            host: 'ls-d8711bbe6a4638679f80de44d1d1ab5bb5e32d45.c28evfenbknb.us-east-1.rds.amazonaws.com',
+            port: parseInt(DB_PORT),
             user: DB_USER,
             password: DB_PASSWORD, // Reemplaza por tu contraseña real
             database: DB_NAME
